b6ca23f64782c76047ad1899e7799fd73e6a2be1
royalguy52 <royalguy52@gmail.com>
2025-12-04 22:48:14 +0400

    health files changed to update proper enviroment

--- app/Controllers/Api/Health.php
+++ app/Controllers/Api/Health.php
@@ -8,10 +8,13 @@ class Health extends ResourceController
 {
     public function index()
     {
+        $env = env('CI_ENVIRONMENT', 'production');
+
         return $this->respond([
-            'status'  => 'ok',
-            'message' => 'Tilawat API is running (local)',
-            'time'    => date('c'),
+            'status'      => 'ok',
+            'message'     => 'Tilawat API is running',
+            'environment' => $env,
+            'time'        => date('c'),
         ]);
     }
 }
